126858b027ceb909943cbbe001fed50edfb61b68
Sergei Kiryutkin <kiryutkin88@yandex.ru>
2024-07-25 15:10:34 +0300

    new

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".mySwiper", {
+    effect: "fade",
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
